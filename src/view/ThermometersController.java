7cf40ce474d62b3dc5065ab0b7a8232955be3519
stefaniatomuta <304173@via.dk>
2021-03-01 14:23:16 +0100

    view

--- src/view/ThermometersController.java
+++ src/view/ThermometersController.java
@@ -3,6 +3,7 @@ package view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import viewmodel.ThermometersViewModel;
 
 public class ThermometersController
 {
@@ -13,18 +14,29 @@ public class ThermometersController
 
     private Region root;
     private ViewHandler viewHandler;
+    private ThermometersViewModel thermometersViewModel;
 
     public ThermometersController(){
 
     }
-    public void init(ViewHandler viewHandler, Region root){
+    public void init(ViewHandler viewHandler, ThermometersViewModel thermometersViewModel, Region root){
         this.viewHandler = viewHandler;
+        this.thermometersViewModel = thermometersViewModel;
+        this.root= root;
+
+        thermometer0.textProperty().bind(thermometersViewModel.thermometer0Property());
+        thermometer1.textProperty().bind(thermometersViewModel.thermometer1Property());
+        thermometer2.textProperty().bind(thermometersViewModel.thermometer2Property());
+        errorLabel.textProperty().bind(thermometersViewModel.errorLabelProperty());
     }
     public Region getRoot(){
         return root;
     }
     @FXML private void settingsClicked(){
 
+    }
+    public void reset(){
+        //dsjhgsds
     }
 
 
--- src/view/ViewHandler.java
+++ src/view/ViewHandler.java
@@ -1,4 +1,69 @@
 package view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
+import viewmodel.ViewModelFactory;
+
 public class ViewHandler {
+
+    private Scene currentScene;
+    private Stage primaryStage;
+    private ViewModelFactory viewModelFactory;
+    private ThermometersController thermometersController;
+
+    public ViewHandler(ViewModelFactory viewModelFactory){
+        this.viewModelFactory = viewModelFactory;
+        currentScene = new Scene(new Region());
+    }
+    public void start(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        openView("thermometers");
+    }
+    public void openView(String id)
+    {
+        Region root = null;
+        switch (id)
+        {
+            case "thermometers":
+                root = loadThermometersView("FirstWindowThermometer.fxml");
+        }
+        currentScene.setRoot(root);
+        String title = "";
+        if (root.getUserData() != null)
+        {
+            title += root.getUserData();
+        }
+        primaryStage.setTitle(title);
+        primaryStage.setScene(currentScene);
+        primaryStage.setWidth(root.getPrefWidth());
+        primaryStage.setHeight(root.getPrefHeight());
+        primaryStage.show();
+    }
+
+    public void closeView()
+    {
+        primaryStage.close();
+    }
+
+    public Region loadThermometersView(String fxmlFile){
+        if(thermometersController == null){
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource(fxmlFile));
+                Region root = loader.load();
+                thermometersController = loader.getController();
+                thermometersController.init(this, viewModelFactory.getFirstWindowThermometerViewModel(),root);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        else{
+            thermometersController.reset();
+        }
+        return thermometersController.getRoot();
+    }
+
 }
