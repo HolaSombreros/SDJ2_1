570e15cddde26f469c7fe66ebc90ea3101a3345c
Sombrero <adriana@grecea.net>
2021-03-01 15:01:15 +0100

    fxml

--- src/view/SettingsViewController.java
+++ src/view/SettingsViewController.java
@@ -1,5 +1,60 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
+import viewmodel.SettingsViewModel;
+
+
+
 public class SettingsViewController
 {
+    //private instance variables
+    private Region root;
+    private ViewHandler viewHandler;
+    private SettingsViewModel viewModel;
+    @FXML private TextField highValueField;
+    @FXML private TextField lowValueField;
+    @FXML private Label highValueLabel;
+    @FXML private Label lowValueLabel;
+    @FXML private Label errorLabel;
+    @FXML private Label radiatorStateLabel;
+
+    public SettingsViewController() {
+    }
+
+
+    public void init(ViewHandler viewHandler, SettingsViewModel viewModel, Region root){
+            this.viewHandler = viewHandler;
+            this.viewModel = viewModel;
+            this.root = root;
+            //TODO: bind labels and textFields
+    }
+
+    public void reset(){
+
+    }
+
+    public Region getRoot() {
+        return root;
+    }
+
+    @FXML public void turnUp(){
+
+    }
+
+    @FXML public void turnDown(){
+
+    }
+
+    @FXML public void cancel(){
+
+    }
+
+    @FXML public void set(){
+
+    }
+
+
 }
--- src/viewmodel/SettingsViewModel.java
+++ src/viewmodel/SettingsViewModel.java
@@ -1,5 +1,49 @@
 package viewmodel;
 
-public class SettingsViewModel
-{
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import mediator.Model;
+
+public class SettingsViewModel {
+
+    //private attributes
+
+    private IntegerProperty highValueProperty;
+    private IntegerProperty lowValueProperty;
+    private StringProperty radiatorStateProperty;
+    private StringProperty errorProperty;
+    private Model model;
+
+
+    /**
+     * @param model
+     * */
+    public SettingsViewModel(Model model) {
+        this.model = model;
+        highValueProperty = new SimpleIntegerProperty();
+        highValueProperty = new SimpleIntegerProperty();
+        radiatorStateProperty = new SimpleStringProperty();
+        errorProperty = new SimpleStringProperty();
+    }
+
+
+    public IntegerProperty highValueProperty() {
+        return highValueProperty;
+    }
+
+    public IntegerProperty lowValueProperty() {
+        return lowValueProperty;
+    }
+
+
+    public StringProperty radiatorStateProperty() {
+        return radiatorStateProperty;
+    }
+
+
+    public StringProperty errorProperty() {
+        return errorProperty;
+    }
 }
